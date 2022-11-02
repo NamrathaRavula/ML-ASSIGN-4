91dad105b2f070b3afd2e218c87eafa15d392a83
NamrathaRavula <112675995+NamrathaRavula@users.noreply.github.com>
2022-11-01 23:12:22 -0500

    Add files via upload

---
+++ assignment-ml4.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
+from sklearn.cluster import KMeans
+from sklearn.impute import SimpleImputer
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+sns.set(style="white", color_codes=True)
+import warnings
+warnings.filterwarnings("ignore")
+df=pd.read_csv("C:/Users/namra/Downloads/datasets/datasets/Salary_Data.csv")
+df.head()
+X = df.iloc[:, :-1].values
+Y = df.iloc[:, 1].values
+X_Train, X_Test, Y_Train, Y_Test = train_test_split(X,Y, test_size=1/3,random_state = 0)
+regressor = LinearRegression()
+regressor.fit(X_Train, Y_Train)
+Y_Pred = regressor.predict(X_Test)
+mean_squared_error(Y_Test,Y_Pred)
+plt.title('Training data')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.scatter(X_Train, Y_Train)
+plt.show()
+plt.title('Testing data')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.scatter(X_Test, Y_Test)
+plt.show()
+
+
+
+
+
+df2=pd.read_csv("C:/Users/namra/Downloads/datasets/datasets/K-Mean_Dataset.csv")
+df2.head()
+X = df2.iloc[:,1:].values
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(X)
+X = imputer.transform(X)
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('the elbow method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Wcss')
+plt.show()
+nclusters = 4 # this is the k in kmeans
+km = KMeans(n_clusters=nclusters)
+km.fit(X)
+y_cluster_kmeans = km.predict(X)
+from sklearn import metrics
+score = metrics.silhouette_score(X, y_cluster_kmeans)
+print('Silhouette score:',score)
+
+
+scaler = preprocessing.StandardScaler()
+scaler.fit(X)
+X_scaled_array = scaler.transform(X)
+X_scaled = pd.DataFrame(X_scaled_array)
+from sklearn.cluster import KMeans
+nclusters = 2 
+km = KMeans(n_clusters=nclusters)
+km.fit(X_scaled)
+y_scaled_cluster_kmeans = km.predict(X_scaled)
+from sklearn import metrics
+score = metrics.silhouette_score(X_scaled, y_scaled_cluster_kmeans)
+print('Silhouette score after applying scaling:',score)
+
+
+
